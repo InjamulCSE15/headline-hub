c699ec48553deb6b43c26347f15e7963fd3bfcaa
unknown <yeasin.mohammod018@gmail.com>
2025-04-18 11:59:38 +0600

    env implementation

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCBTjLEU4eHf19yygtSV6HF86dsyHGNkTA",
-  authDomain: "headline-hub-da82a.firebaseapp.com",
-  projectId: "headline-hub-da82a",
-  storageBucket: "headline-hub-da82a.firebasestorage.app",
-  messagingSenderId: "452050786122",
-  appId: "1:452050786122:web:3b21c8678863454abd93b3"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
